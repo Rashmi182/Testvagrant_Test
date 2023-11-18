c17434dfe2ada1ee7d9435cfbd55916bfc9e440c
Rashmi182 <145493877+Rashmi182@users.noreply.github.com>
2023-11-18 02:40:10 -0500

    Add files via upload

---
+++ Shopping_cart.py
@@ -0,0 +1,16 @@
+# Creating DS
+a=int(input("Product: Leather wallet, Umbrella, Cigarette, Honey\t Unit price in rs: 1100, 900, 200, 100\t  GST in %:18, 12, 28, 0\t  Quantity:1, 4, 3, 2"))
+printf(a)
+
+# Calc Total amount
+v1=1100 
+v2=900
+v3=200
+v4=100
+sum=v1+v2+v3+v4
+print(sum)
+
+# Max GST
+p=[18, 12, 28, 0] 
+q=max(p)
+print(q)
